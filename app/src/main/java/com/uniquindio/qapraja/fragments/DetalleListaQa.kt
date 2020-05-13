dc1e30e385fb6352ed524d5a4c5bde26782b3298
mariahlejita-dg <51831521+mariahlejita-dg@users.noreply.github.com>
2020-05-13 15:31:13 -0500

    MM

--- app/src/main/java/com/uniquindio/qapraja/fragments/DetalleListaQa.kt
+++ app/src/main/java/com/uniquindio/qapraja/fragments/DetalleListaQa.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.CheckBox
 
 import com.uniquindio.praja20.vo.Job
 import com.uniquindio.qapraja.R
@@ -16,31 +18,38 @@ import kotlinx.android.synthetic.main.fragment_detalle_lista_qa.*
 class DetalleListaQa : Fragment() {
 
     var jobDetalle: Job?=null
+    lateinit var button: Button
+    lateinit var checkBox: CheckBox
+     lateinit var v: View
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detalle_lista_qa, container, false)
+        v = inflater.inflate(R.layout.fragment_detalle_lista_qa, container, false)
+        button = v.findViewById(R.id.btnChange)
+        checkBox = v.findViewById(R.id.finishCheckBox)
+        return v
     }
 
     fun darDetallwe(job: Job){
         this.jobDetalle = job
-        numJobQa.text = job.numberJob
-        jobNameqa.text = job.title
-        customerqa.text = job.customer
-        mulqa.text = job.mul_job
-        qtyqa.text = job.qty
-        pageCountqa.text = job.pageCount
-        cvqa.text = job.cv
-        textqa.text = job.text
-        formatqa.text = job.format
-        stockqa.text = job.stock
-        inkqa.text = job.ink
-        finishSizeqa.text = job.finisSize
-        bindingqa.text = job.binding
-        dueDateqa.text = job.dueDate
-        estadoqa.text = job.estado
-        commentsqa.text = job.comment
+        numberJob_process.text = job.numberJob
+        jobName_process.text = job.title
+        customer_process.text = job.customer
+        mul_process.text = job.mul_job
+        qty_process.text = job.qty
+        pageCount_process.text = job.pageCount
+        cv_process.text = job.cv
+        text_process.text = job.text
+        format_process.text = job.format
+        stock_process.text = job.stock
+        ink_process.text = job.ink
+        finishSize_process.text = job.finisSize
+        binding_process.text = job.binding
+        dueDate_process.text = job.dueDate
+        estado_process.text = job.estado
+        comments_process.text = job.comment
     }
+
 }
